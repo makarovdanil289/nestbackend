34c3f7ca1c768847c843943ec1a3b9df2f9a6869
Danil <makarovdanil289@gmail.com>
2023-10-09 20:39:32 +0500

    init repository

---
+++ src/moks/index.ts
@@ -0,0 +1,228 @@
+/* eslint-disable prettier/prettier */
+export const users = [
+    {
+      "_id": "651d45b6fb1fd8a39457b8ca",
+      "index": 0,
+      "guid": "bc2d535f-4e58-490a-9b43-8298e4690178",
+      "isActive": false,
+      "balance": "$2,734.93",
+      "picture": "http://placehold.it/32x32",
+      "age": 33,
+      "eyeColor": "brown",
+      "name": "Barbara Acosta",
+      "gender": "female",
+      "company": "COMCUBINE",
+      "email": "barbaraacosta@comcubine.com",
+      "phone": "+1 (848) 509-3007",
+      "address": "319 Green Street, Fingerville, Marshall Islands, 7084",
+      "about": "Nostrud do nisi labore consectetur cupidatat nostrud sunt sint. Veniam dolor cillum et nisi ex consectetur ea labore nulla reprehenderit adipisicing qui. Consequat aliqua sint do anim. Mollit exercitation laboris do elit nulla consectetur elit aute ut aliquip duis laborum ad enim.\r\n",
+      "registered": "2019-05-21T02:43:31 -05:00",
+      "latitude": -3.027599,
+      "longitude": 74.658033,
+      "tags": [
+        "consectetur",
+        "deserunt",
+        "magna",
+        "Lorem",
+        "occaecat",
+        "excepteur",
+        "sint"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Rosemary Baker"
+        },
+        {
+          "id": 1,
+          "name": "Merle Mcintyre"
+        },
+        {
+          "id": 2,
+          "name": "Wells Hall"
+        }
+      ],
+      "greeting": "Hello, Barbara Acosta! You have 9 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "651d45b6f61a1041070fec07",
+      "index": 1,
+      "guid": "c9d1beb5-117d-493b-b576-ac8f36bbeabc",
+      "isActive": true,
+      "balance": "$1,201.66",
+      "picture": "http://placehold.it/32x32",
+      "age": 20,
+      "eyeColor": "brown",
+      "name": "Vivian Nelson",
+      "gender": "female",
+      "company": "XANIDE",
+      "email": "viviannelson@xanide.com",
+      "phone": "+1 (944) 494-3775",
+      "address": "401 Montana Place, Strong, Tennessee, 9942",
+      "about": "Amet pariatur veniam elit nostrud aute pariatur dolor enim excepteur. Laboris eu aute aliqua sunt dolor fugiat ipsum anim. Officia aliquip consectetur consequat anim deserunt est aliqua eu pariatur deserunt. Adipisicing ipsum anim deserunt consectetur dolore eiusmod ad consequat ea fugiat.\r\n",
+      "registered": "2020-11-28T06:31:47 -05:00",
+      "latitude": 83.035896,
+      "longitude": 7.570257,
+      "tags": [
+        "dolor",
+        "quis",
+        "ullamco",
+        "adipisicing",
+        "sunt",
+        "sint",
+        "veniam"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Francisca Blevins"
+        },
+        {
+          "id": 1,
+          "name": "Yolanda Horne"
+        },
+        {
+          "id": 2,
+          "name": "Rowland Brown"
+        }
+      ],
+      "greeting": "Hello, Vivian Nelson! You have 10 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "651d45b62ee386616711e816",
+      "index": 2,
+      "guid": "57b71ab0-6b44-44cb-9938-f9e67b4a2610",
+      "isActive": false,
+      "balance": "$2,278.00",
+      "picture": "http://placehold.it/32x32",
+      "age": 35,
+      "eyeColor": "blue",
+      "name": "Lorene James",
+      "gender": "female",
+      "company": "ROCKYARD",
+      "email": "lorenejames@rockyard.com",
+      "phone": "+1 (890) 535-3855",
+      "address": "362 Madeline Court, Stollings, Indiana, 8936",
+      "about": "Irure occaecat ut laboris incididunt adipisicing ea pariatur elit non sint ad. Laboris anim deserunt consectetur Lorem occaecat culpa et ea eiusmod enim. Ut anim mollit proident enim non tempor eiusmod. Aliqua ullamco eiusmod proident cupidatat est laborum laboris dolore commodo veniam ullamco et tempor.\r\n",
+      "registered": "2020-01-13T10:49:23 -05:00",
+      "latitude": -24.429493,
+      "longitude": -126.357885,
+      "tags": [
+        "minim",
+        "laborum",
+        "ullamco",
+        "cupidatat",
+        "minim",
+        "ullamco",
+        "occaecat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Elisa Payne"
+        },
+        {
+          "id": 1,
+          "name": "Mann Morales"
+        },
+        {
+          "id": 2,
+          "name": "Huffman Rutledge"
+        }
+      ],
+      "greeting": "Hello, Lorene James! You have 9 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "651d45b66d6d62edc44bdcbb",
+      "index": 3,
+      "guid": "555711a6-7613-4d5f-997e-d6cd0dbd7232",
+      "isActive": false,
+      "balance": "$3,830.46",
+      "picture": "http://placehold.it/32x32",
+      "age": 30,
+      "eyeColor": "blue",
+      "name": "Cathryn Garrison",
+      "gender": "female",
+      "company": "NAXDIS",
+      "email": "cathryngarrison@naxdis.com",
+      "phone": "+1 (863) 465-3886",
+      "address": "440 Battery Avenue, Southview, Rhode Island, 7878",
+      "about": "Lorem sunt sint nisi laborum aliquip aliquip amet cupidatat mollit irure quis sit culpa. Enim aliqua exercitation dolore ex mollit mollit velit exercitation. Qui Lorem minim laborum et qui magna nisi cillum ex adipisicing quis reprehenderit.\r\n",
+      "registered": "2019-12-20T08:39:07 -05:00",
+      "latitude": 80.401849,
+      "longitude": -171.356962,
+      "tags": [
+        "aliquip",
+        "minim",
+        "ad",
+        "proident",
+        "mollit",
+        "dolor",
+        "qui"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Sondra Bradshaw"
+        },
+        {
+          "id": 1,
+          "name": "Katie Frazier"
+        },
+        {
+          "id": 2,
+          "name": "Gallagher Wood"
+        }
+      ],
+      "greeting": "Hello, Cathryn Garrison! You have 9 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "651d45b64ff5e0342eec4b31",
+      "index": 4,
+      "guid": "f5d1c965-b9c8-4244-bb47-e5842e026aac",
+      "isActive": true,
+      "balance": "$2,337.55",
+      "picture": "http://placehold.it/32x32",
+      "age": 30,
+      "eyeColor": "brown",
+      "name": "Melba Hobbs",
+      "gender": "female",
+      "company": "ENOMEN",
+      "email": "melbahobbs@enomen.com",
+      "phone": "+1 (993) 431-3153",
+      "address": "353 Chester Court, Detroit, Maryland, 7294",
+      "about": "Nulla eiusmod velit irure laborum labore esse do ad tempor voluptate voluptate duis voluptate veniam. Esse magna cupidatat eiusmod ex consequat proident. Non pariatur ad in quis cupidatat. Cillum reprehenderit excepteur aliqua magna consequat sunt velit laborum est ad laborum nostrud Lorem eu. Nostrud elit ullamco consectetur ad reprehenderit tempor sunt magna esse dolore in esse.\r\n",
+      "registered": "2020-10-06T10:49:39 -05:00",
+      "latitude": 7.838775,
+      "longitude": 62.093636,
+      "tags": [
+        "est",
+        "quis",
+        "mollit",
+        "culpa",
+        "ad",
+        "laborum",
+        "consequat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Angeline Velez"
+        },
+        {
+          "id": 1,
+          "name": "Melton Hendrix"
+        },
+        {
+          "id": 2,
+          "name": "Marian Cardenas"
+        }
+      ],
+      "greeting": "Hello, Melba Hobbs! You have 8 unread messages.",
+      "favoriteFruit": "apple"
+    }
+  ]
---
+++ src/user/users.service.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { users } from '../moks';
+
+@Injectable()
+export class UsersService {
+    getUsers() {
+        return users;
+    }
+}
