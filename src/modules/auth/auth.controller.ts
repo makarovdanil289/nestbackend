f81ac3edcb8eff38f2cce3075f9778fdceb2fb47
Danil <makarovdanil289@gmail.com>
2023-10-13 00:42:38 +0500

    Api documentation. Add rout Auth in documentation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,16 +5,21 @@ import { CreateUserDTO } from '../user/dto';
 import { AuthService } from './auth.service';
 import { UserLoginDTO } from './dto';
 import { AuthUserResponse } from './response';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    @ApiTags('API')
+    @ApiResponse({status: 201, type: CreateUserDTO})
     @Post('register')
     register(@Body() dto: CreateUserDTO): Promise<CreateUserDTO> {
         return this.authService.registerUsers(dto)
     }
 
+    @ApiTags('API')
+    @ApiResponse({status: 200, type: AuthUserResponse})
     @Post('login')
     login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse> {
         return this.authService.loginUser(dto)
--- src/modules/user/dto/index.ts
+++ src/modules/user/dto/index.ts
@@ -1,16 +1,21 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from "@nestjs/swagger";
 import {IsString} from "class-validator";
 
 export class CreateUserDTO {
+    @ApiProperty()
     @IsString()
     firstName: string
 
+    @ApiProperty()
     @IsString()
     username: string
 
+    @ApiProperty()
     @IsString()
     email: string
 
+    @ApiProperty()
     @IsString()
     password: string
 }
